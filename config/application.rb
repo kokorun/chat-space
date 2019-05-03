1a43cdea13aac10d90741af7ba78fa6fa519c33a
kokorun <kokorun24@gmail.com>
2019-05-03 12:01:32 +0900

    update

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,7 @@ module ChatSpace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  public/uploads/*
 end
+end
+
+# public/uploads/*
